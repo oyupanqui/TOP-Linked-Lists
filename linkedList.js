331ab17dd5f88ffb48dbc0b20f819cc255991319
oyupanqui <osmar30asis@gmail.com>
2024-06-25 17:31:20 -0500

    created classes linkedlist and node

---
+++ linkedList.js
@@ -0,0 +1,14 @@
+class LinkedList {
+    constructor () {
+        this.head = null
+        this.tail = null
+        this.size = 0
+    }
+}
+
+class Node {
+    constructor () {
+        this.value = null
+        this.next = null
+    }
+}
